ce7cccd14e18e4919dece7cb321feeccd3f57586
JordanTseng <jordan.ttc.design@gmail.com>
2022-04-19 23:38:23 +0800

    新增篩選框

--- src/views/PostsPage.vue
+++ src/views/PostsPage.vue
@@ -25,8 +25,7 @@ export default {
           'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
         creatAt: '2022/1/10 12:00',
         postContent: '<p>我一定要成為很棒棒的國王</p><p>搶到想要的 NFT 啦！ya~~</p>',
-        postImgUrl:
-          '',
+        postImgUrl: '',
       },
       {
         _id: '456',
@@ -35,8 +34,7 @@ export default {
           'https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
         creatAt: '2022/1/10 12:00',
         postContent: '<p>各位我有一個計畫</p><p>搶到想要的 NFT 啦！ya~~</p>',
-        postImgUrl:
-          '',
+        postImgUrl: '',
       },
     ]);
     return { postList };
@@ -47,6 +45,23 @@ export default {
   <div class="container">
     <div class="row d-flex justify-content-center">
       <div class="col-7">
+        <div class="d-flex gap-3 mb-4">
+          <select class="selectTool form-select">
+            <option selected value="最新">最新貼文</option>
+          </select>
+          <div class="input-group inputToolContainer flex-grow-1">
+            <input
+              type="text"
+              class="form-control inputTool"
+              placeholder="搜尋貼文"
+              aria-label="Username"
+              aria-describedby="basic-addon1"
+            />
+            <button class="btn btn-primary" type="button">
+              <i class="bi bi-search text-white"></i>
+            </button>
+          </div>
+        </div>
         <div class="d-flex flex-column gap-4">
           <template v-for="postItem in postList" :key="postItem._id">
             <PostCard :post-item="postItem" />
@@ -57,41 +72,4 @@ export default {
   </div>
 </template>
 <style lang="scss" scoped>
-.postBox {
-  display: flex;
-  flex-direction: column;
-  padding: 1.5rem;
-  border-radius: 0.5rem;
-  border: 2px solid #000;
-  box-shadow: 0px 3px 0px #000400;
-}
-.userBox {
-  display: flex;
-  &__pic {
-    width: 45px;
-    height: 45px;
-    border-radius: 50%;
-    border: 2px solid #000;
-    object-fit: cover;
-  }
-  &__title {
-    font-weight: bold;
-    color: #000;
-  }
-  &__subTxt {
-    font-size: 0.75rem;
-    color: #9b9893;
-  }
-}
-.postBox__content {
-  display: flex;
-  flex-direction: column;
-  &__img {
-    max-height: 10rem;
-    width: 100%;
-    object-fit: cover;
-    border: 2px solid #000;
-    border-radius: 0.5rem;
-  }
-}
 </style>
