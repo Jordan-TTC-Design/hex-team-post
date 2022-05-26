1c09132306b645388005cbfae7b6a72e4b061a15
JordanTseng <jordan.ttc.design@gmail.com>
2022-05-26 17:47:34 +0800

    修改：後台貼文頁面樣式

--- src/stores/backstage/backStatusStore.js
+++ src/stores/backstage/backStatusStore.js
@@ -9,7 +9,7 @@ const backStatusStore = defineStore({
       { name: '會員管理', link: 'member' },
       { name: '日記作者管理', link: 'auther' },
       { name: '貼文管理', link: 'post' },
-      { name: '私密日記管理', link: 'member' },
+      { name: '私密日記管理', link: 'post' },
       { name: '銷售管理', link: 'member' },
       { name: '設定 ＆ 其他', link: 'member' },
     ],
--- src/views/backstage/PostPage.vue
+++ src/views/backstage/PostPage.vue
@@ -1,315 +1,143 @@
 <script>
+import { ref } from 'vue';
+import backStatusStore from '@/stores/backstage/backStatusStore';
+
 export default {
   setup() {
-    return {};
+    const backstatusData = backStatusStore();
+    const pageSubNav = ref('刊登中');
+    backstatusData.nowPage = '貼文管理';
+    return {
+      backstatusData,
+      pageSubNav,
+    };
   },
 };
 </script>
 
 <template>
-  <div class="title border-bottom">貼文管理</div>
-  <div class="content">
-    <div class="d-flex align-items-center mb-3">
-      <div class="btn me-3">刊登</div>
-      <div class="btn me-3">下架</div>
-      <div class="btn ms-auto">搜尋, 下拉選單</div>
+  <div class="backPageHeader border-bottom">
+    <div class="backPageHeader__titleBox">
+      <h2 class="backPageHeader__titleBox__title">{{ backstatusData.nowPage }}</h2>
     </div>
-    <div class="row row-cols-4">
+    <div class="bg-white p-3 border-top">
+      <div class="subNav--row gap-3">
+        <div :class="{ active: pageSubNav === '刊登中' }" class="subNav--row__item">刊登中</div>
+        <div :class="{ active: pageSubNav === '已下架' }" class="subNav--row__item">已下架</div>
+        <div class="subNav--row__item ms-auto">搜尋, 下拉選單</div>
+      </div>
+    </div>
+  </div>
+  <div class="backStageMainContent">
+    <div class="row row-cols-3">
       <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
+        <div class="backPostCard h-100">
+          <div class=" d-flex align-items-center p-3 pb-0">
+            <div class="user-picture"></div>
+            <div class="user-info">
+              <span class="user-info-title">卡斯伯</span>
+              <span class="user-info-subtitle">2022 / 01 01 12:12</span>
             </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
+          </div>
+          <div class="card__body d-flex flex-column gap-3 mb-2 flex-grow-1">
+            <p class="backPostCard__txtContent">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus itaque enim quae
+              necessitatibus, at, explicabo obcaecati maiores aspernatur, nulla impedit facilis iste
+              eaque quis vitae sunt sint eveniet expedita labore? Asperiores commodi totam corrupti
+              accusantium! Est tempore nemo placeat! Perferendis fugit itaque culpa saepe.
+              Perspiciatis libero nihil tempore enim, quae beatae cum aliquam, officia dolores eos,
+              quos cumque expedita inventore.
+            </p>
+          </div>
+          <div class="d-flex justify-content-between ps-3">
+            <ul class="iconInfoList">
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-heart"></i>325</li>
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-eye"></i>20</li>
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-chat-left-dots"></i>20</li>
+            </ul>
+            <button type="button" class="btn"><i class="bi bi-three-dots"></i></button>
           </div>
         </div>
       </div>
       <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
+        <div class="backPostCard h-100">
+          <div class=" d-flex align-items-center p-3 pb-0">
+            <div class="user-picture"></div>
+            <div class="user-info">
+              <span class="user-info-title">卡斯伯</span>
+              <span class="user-info-subtitle">2022 / 01 01 12:12</span>
             </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
+          </div>
+          <div class="card__body d-flex flex-column gap-3 mb-2 flex-grow-1">
+            <p class="backPostCard__txtContent backPostCard__txtContent--fold">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus itaque enim quae
+              necessitatibus, at, explicabo obcaecati maiores aspernatur, nulla impedit facilis iste
+              eaque quis vitae sunt sint eveniet expedita labore? Asperiores commodi totam corrupti
+              accusantium! Est tempore nemo placeat! Perferendis fugit itaque culpa saepe.
+              Perspiciatis libero nihil tempore enim, quae beatae cum aliquam, officia dolores eos,
+              quos cumque expedita inventore.
+            </p>
+            <img
+              class="backPostCard__imgContent"
+              src="https://images.unsplash.com/photo-1653515906416-0a4c47eb5d20?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674"
+              alt="貼文圖"
+            />
+          </div>
+          <div class="d-flex justify-content-between ps-3">
+            <ul class="iconInfoList">
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-heart"></i>325</li>
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-eye"></i>20</li>
+              <li class="iconInfoList__item"><i class="webIcon--sm bi bi-chat-left-dots"></i>20</li>
+            </ul>
+            <button type="button" class="btn"><i class="bi bi-three-dots"></i></button>
           </div>
         </div>
       </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col mb-3">
-        <div class="card">
-          <div class="card-body">
-            <div class="d-flex align-items-center mb-3">
-              <div class="user-picture"></div>
-              <div class="user-info">
-                <span class="user-info-title">卡斯伯</span>
-                <span class="user-info-subtitle">2022 / 01 01 12:12</span>
-              </div>
-            </div>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-            itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-            aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-            eveniet expedita labore? Asperiores commodi totam corrupti
-            accusantium! Est tempore nemo placeat! Perferendis fugit itaque
-            culpa saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-            aliquam, officia dolores eos, quos cumque expedita inventore.
-            <div class="d-flex align-items-center mt-3">
-              <span class="me-3">325</span>
-              <span class="me-3">20</span>
-              <span class="me-3">20</span>
-              <span class="ms-auto">more</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- <div class="postCard mx-2 mt-2">
-        <div class="postCard-header">
-          <span class="title2">卡斯伯</span>
-          <span class="subTitle">2022 / 01 01 12:12</span>
-        </div>
-        <div class="postCard-content">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
-          itaque enim quae necessitatibus, at, explicabo obcaecati maiores
-          aspernatur, nulla impedit facilis iste eaque quis vitae sunt sint
-          eveniet expedita labore? Asperiores commodi totam corrupti
-          accusantium! Est tempore nemo placeat! Perferendis fugit itaque culpa
-          saepe. Perspiciatis libero nihil tempore enim, quae beatae cum
-          aliquam, officia dolores eos, quos cumque expedita inventore.
-        </div>
-        <div class="postCard-footer">
-          <span>325</span>
-          <span>20</span>
-          <span>20</span>
-          <span class="ms-auto">more</span>
-        </div>
-      </div>
-         -->
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.content {
-  padding: 10px 20px;
+.backStageMainContent {
+  padding: 1.5rem 1.125rem;
 }
-
-.tmp {
-  margin-right: -20px;
+.backPostCard {
+  background-color: var(--bs-white);
+  border-radius: 0.75rem;
+  box-shadow: 0px 1px 2px #00000029;
+  display: flex;
+  flex-direction: column;
+  cursor: pointer;
+  .card__body {
+    padding: 0.75rem;
+  }
+  &__txtContent {
+    overflow: hidden;
+    height: 12rem;
+    &--fold {
+      max-height: 2.75rem;
+    }
+  }
+  &__imgContent {
+    height: 10rem;
+    width: 100%;
+    object-fit: cover;
+    border-radius: 0.5rem;
+  }
 }
-
-.tmp2 {
-  width: 300px;
+.iconInfoList {
+  display: flex;
+  gap: 0.75rem;
+  &__item {
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    &:hover {
+      color: var(--bs-primary);
+      .webIcon--sm {
+        color: var(--bs-primary);
+      }
+    }
+  }
 }
-
-// .postCard {
-//   padding: 12px;
-//   border-radius: 12px;
-//   width: 268px;
-//   background-color: #fff;
-//   position: relative;
-
-//   .postCard-header {
-//     .title2 {
-//       display: block;
-//       font-size: 14px;
-//       color: #1d1d1d;
-//     }
-//     .subTitle {
-//       display: block;
-//       font-size: 12px;
-//       color: #989898;
-//     }
-//   }
-
-//   .postCard-content {
-//     margin: 12px 0;
-//     overflow: hidden;
-//     text-overflow: ellipsis;
-//     height: 216px;
-//   }
-
-//   .postCard-footer {
-//     display: flex;
-//     margin: 0 -6px;
-//     & > * {
-//       margin: 0 6px;
-//     }
-//   }
-
-//   .badge {
-//     position: absolute;
-//     top: 0;
-//     right: 0;
-//     padding: 8px 0;
-//     width: 36px;
-//     line-height: 20px;
-//     border-top-left-radius: 0;
-//     border-top-right-radius: 12px;
-//     border-bottom-left-radius: 12px;
-//     border-bottom-right-radius: 0;
-//     background-color: #3d24ff;
-//   }
-// }
 </style>
