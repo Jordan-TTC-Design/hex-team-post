34b65b215bd6ffb3516bbae43cf6e5fc1942842f
EQ0846 <skeq0846@skl.com.tw>
2022-05-29 13:41:15 +0800

    新增：追蹤動態頁面使用元件

---
+++ src/components/front/ArticleCard.vue
@@ -0,0 +1,40 @@
+<script>
+// import { ref } from 'vue';
+
+export default {
+  setup(props) {
+    return { props };
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div class="mb-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit eum
+        veniam nemo quidem, expedita enim iure dolore dicta ipsa excepturi
+        ducimus voluptatem! Qui molestiae quos quas cum fugiat numquam pariatur!
+      </div>
+
+      <div class="tmp">
+        <span><i class="bi bi-heart"></i> 123</span>
+        <span class="a">查看文章</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tmp {
+  display: flex;
+  justify-content: space-between;
+  font-size: 12px;
+  color: #2f2f2f;
+}
+
+.a {
+  font-size: 16px;
+  color: #1d1d1d;
+}
+</style>
---
+++ src/components/front/UserCard.vue
@@ -0,0 +1,31 @@
+<script>
+// import { ref } from 'vue';
+
+export default {
+  setup(props) {
+    return { props };
+  },
+};
+</script>
+
+<template>
+  <div class="card" :class="props.class">
+    <div class="card-body">
+      <div class="img-fluid border mb-3 rounded tmp"></div>
+      <div>用戶名稱</div>
+      <div>
+        <div class="icon">
+          <i class="bi bi-heart"></i>
+          <span>123</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.tmp {
+  width: 176px;
+  height: 176px;
+}
+</style>
