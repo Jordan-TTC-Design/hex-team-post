5929f87bdbd38340a7e5de0eb3623953a0d52da5
EQ0846 <skeq0846@skl.com.tw>
2022-05-29 08:43:18 +0800

    修改：個人資料頁面

--- src/views/front/ProfilePage.vue
+++ src/views/front/ProfilePage.vue
@@ -1,29 +1,63 @@
 <script>
 import { ref } from 'vue';
 
-// import PostCard from '@/components/front/PostCard.vue';
-// import DynamicWall from '@/components/front/DynamicWall.vue';
+import PostFilter from '@/components/front/PostFilter.vue';
+import UserProfileCard from '@/components/front/UserProfileCard.vue';
 import postsStore from '@/stores/postsStore';
+import PostCard from '@/components/front/PostCard.vue';
+import SponsorCard from '@/components/front/SponsorCard.vue';
+import PopularPostCard from '@/components/front/PopularPostCard.vue';
+import PersonalCard from '@/components/front/PersonalCard.vue';
+import PersonalEditCard from '@/components/front/PersonalEditCard.vue';
+import DiamondPurchaseRecordCard from '@/components/front/DiamondPurchaseRecordCard.vue';
+import FormRadioButton from '@/components/helper/FormRadioButton.vue';
 
 export default {
   components: {
-    // PostCard,
-    // DynamicWall,
+    PostFilter,
+    UserProfileCard,
+    PostCard,
+    SponsorCard,
+    PopularPostCard,
+    PersonalCard,
+    PersonalEditCard,
+    DiamondPurchaseRecordCard,
+    FormRadioButton,
   },
   setup() {
     const postsData = postsStore();
     const postSort = ref('asc');
     const postQuery = ref('');
+
+    const tabType = ref('POST');
+    const isShowPersonalEditCard = ref(false);
+
     postsData.getPosts();
     function sortPostsData() {
       postsData.getPosts(postSort.value, postQuery.value);
     }
 
+    const handleChangeTab = (newTab) => {
+      tabType.value = newTab;
+    };
+
+    const ShowEditPersonal = () => {
+      isShowPersonalEditCard.value = true;
+    };
+    const HideEditPersonal = () => {
+      isShowPersonalEditCard.value = false;
+    };
+
     return {
       postSort,
       postQuery,
       postsData,
+      tabType,
+      isShowPersonalEditCard,
       sortPostsData,
+      handleChangeTab,
+      ShowEditPersonal,
+      HideEditPersonal,
     };
   },
 };
@@ -32,167 +66,120 @@ export default {
 <template>
   <div class="container d-flex">
     <div class="content">
-      <div class="card userProfileCard mb-3">
-        <div class="card-body userProfileCard-body">
-          <img
-            src="../../assets/image/user-picture.png"
-            alt="user-picture"
-            class="user-picture"
-          />
-          <div class="d-flex flex-column justify-content-center">
-            <span class="userProfileCard-title">用戶名稱</span>
-            <span class="userProfileCard-subtitle">200 人追蹤</span>
-            <span class="userProfileCard-subtitle">200 人追蹤</span>
-          </div>
-          <div class="userProfileCard-more">
-            <button class="btn btn-sm btn-outline text-primary">追蹤</button>
-            <button class="btn btn-sm text-primary">
-              <i class="bi bi-three-dots"></i>
-            </button>
-          </div>
-        </div>
-        <div class="userProfileCard-footer">
-          <div class="userProfileCard-footer-item">貼文</div>
-          <div class="userProfileCard-footer-item">私密日記</div>
-          <div class="userProfileCard-footer-item">追蹤</div>
-        </div>
-      </div>
-      <div class="card mb-3 tmp">
-        <input
-          type="text"
-          class="form-control bg-white search"
-          placeholder="搜尋關鍵字"
-        />
-        <div class="filter">
-          最新貼文
-          <div class="icon">
-            <i class="bi bi-chevron-down"></i>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="d-flex align-items-center mb-3">
-            <img
-              src="../../assets/image/user-picture.png"
-              alt="user-picture"
-              class="user-picture"
-            />
-            <div class="user-info">
-              <span class="user-info-title">用戶名稱</span>
-              <span class="user-info-subtitle">200 人追蹤</span>
-            </div>
-            <div class="btn btn-sm">追蹤</div>
-          </div>
-          <p class="mb-3">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio illo
-            quis reprehenderit consequatur veritatis fugiat recusandae
-            voluptatem similique. Totam non dicta exercitationem, rem soluta
-            minima. Rem perferendis nesciunt suscipit ab! Voluptate perferendis
-            eum, sit iste maiores temporibus accusantium corrupti ducimus itaque
-            ab et nisi magni sed alias illum molestiae ut rem aperiam cupiditate
-            officia eius
-          </p>
-          <img
-            src="https://images.unsplash.com/photo-1614102073832-030967418971?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738"
-            alt=""
-            class="card-picture"
-          />
-          <div class="d-flex align-items-center">
-            <div class="icon text-primary">
-              <i class="bi bi-heart-fill"></i>
-              <span class="">1234</span>
-            </div>
-            <div class="icon">
-              <i class="bi bi-share"></i>
-            </div>
-            <span class="ms-auto">2022 / 01 / 01 12:12</span>
-          </div>
-        </div>
-        <div class="card-body border-top d-flex align-items-center">
-          <div class="user-picture"></div>
-          <input
-            type="text"
-            class="form-control flex-grow-1"
-            placeholder="回覆..."
-          />
-          <div class="btn ms-3">
-            <i class="bi bi-play-fill"></i>
-          </div>
-        </div>
-      </div>
+      <UserProfileCard :tabType="tabType" @change-tab="handleChangeTab" />
 
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="d-flex align-items-center mb-3">
-            <div class="user-picture"></div>
-            <div class="user-info">
-              <span class="user-info-title">用戶名稱</span>
+      <template v-if="tabType === 'POST'">
+        <PostFilter class="mb-3" />
+        <PostCard />
+        <PostCard />
+      </template>
+      <template v-if="tabType === 'DIARY'">
+        <div class="d-flex mb-3">
+          <FormRadioButton class="me-3" name="type">全部</FormRadioButton>
+          <FormRadioButton class="me-3" name="type">永恆日記</FormRadioButton>
+          <PostFilter class="flex-grow-1" />
+        </div>
+        <PostCard />
+        <PostCard />
+      </template>
+      <template v-if="tabType === 'FOLLOW'">
+        <div class="card mb-3">
+          <div class="card-body">
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
             </div>
-            <div class="btn btn-sm text-primary">追蹤</div>
-          </div>
-          <div class="d-flex align-items-center mb-3">
-            <div class="user-picture"></div>
-            <div class="user-info">
-              <span class="user-info-title">用戶名稱</span>
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
             </div>
-            <div class="btn btn-sm text-primary">追蹤</div>
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
+            </div>
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
+            </div>
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
+            </div>
+            <div class="d-flex align-items-center mb-3 py-2">
+              <div class="user-picture"></div>
+              <div class="user-info">
+                <span class="user-info-title">用戶名稱</span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </template>
+      <template v-if="tabType === 'LIKE'">
+        <PostFilter class="mb-3" />
+        <PostCard />
+        <PostCard />
+      </template>
+      <template v-if="tabType === 'WALLET'">
+        <div class="d-flex">
+          <div class="subSide fix">
+            <div class="side-menu-item mb-3">日記購買紀錄</div>
+            <div class="side-menu-item mb-3">日記銷售紀錄</div>
+            <div class="side-menu-item mb-3 active">鑽石購買紀錄</div>
+          </div>
+          <div class="subContent">
+            <DiamondPurchaseRecordCard />
+            <DiamondPurchaseRecordCard />
+            <DiamondPurchaseRecordCard />
+            <DiamondPurchaseRecordCard />
+          </div>
+        </div>
+      </template>
+      <template v-if="tabType === 'SETTING'">
+        <div class="d-flex">
+          <div class="subSide fix">
+            <div class="side-menu-item mb-3 active">個人資料</div>
+            <div class="side-menu-item mb-3">變更密碼</div>
+            <div class="side-menu-item mb-3">帳戶資料</div>
+          </div>
+          <div class="subContent">
+            <PersonalCard class="mb-3" @show-edit="ShowEditPersonal" />
+            <PersonalEditCard
+              v-if="isShowPersonalEditCard"
+              @hide-edit="HideEditPersonal"
+            />
+          </div>
+        </div>
+      </template>
     </div>
     <div class="side fix">
-      <div class="card mb-3">
-        <div class="card-body border-bottom">
-          <div class="card-title">贊助方案</div>
-        </div>
-        <div class="card-body">
-          <div class="sponsor">
-            <span class="sponsor-title">NTD 1001 / 月</span>
-            <span class="sponsor-subtitle">當月私密日記無限觀看</span>
-          </div>
-          <div class="sponsor">
-            <span class="sponsor-title">NTD 1001 / 月</span>
-            <span class="sponsor-subtitle">當月私密日記無限觀看</span>
-          </div>
-          <div class="sponsor active">
-            <span class="sponsor-title">NTD 1001 / 月</span>
-            <span class="sponsor-subtitle">當月私密日記無限觀看</span>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-3">
-        <div class="card-body border-bottom">
-          <div class="card-title">創作者熱門貼文</div>
-        </div>
-        <div class="card-body"></div>
-      </div>
+      <SponsorCard />
+      <PopularPostCard />
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.tmp {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 44px;
+.side-menu-item {
+  padding: 10px 0;
+  text-align: center;
+  font-size: 16px;
+  columns: #1d1d1d;
+  background: #fff;
+  border-radius: 12px;
 
-  .filter {
-    width: 180px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    position: relative;
-    padding-left: 12px;
-    &::before {
-      content: '';
-      border-left: 1px solid #dee2e6;
-      position: absolute;
-      width: 1px;
-      height: 20px;
-      left: 0;
-    }
+  &.active {
+    background: var(--bs-secondary);
+    color: var(--bs-primary);
   }
 }
 </style>
