c4f3b733ace7ad7d75b93131aa9ed2344da9f09b
EQ0846 <skeq0846@skl.com.tw>
2022-05-29 08:36:40 +0800

    新增：Checkbox，RadioButton 元件

---
+++ src/components/helper/FormRadioButton.vue
@@ -0,0 +1,59 @@
+<script>
+export default {
+  // props: ['', 'input-id', 'input-type'],
+  props: {
+    class: String,
+    name: String,
+  },
+  setup(props, { emit }) {
+    const onChange = (e) => {
+      emit('aaaa', e);
+    };
+
+    return {
+      props,
+      onChange,
+    };
+  },
+};
+</script>
+
+<template>
+  <label class="form-checkbox" :class="props.class">
+    <input type="radio" :name="props.name" @change="onChange" />
+    <span>
+      <slot></slot>
+    </span>
+  </label>
+</template>
+
+<style lang="scss" scoped>
+.form-checkbox {
+  display: inline-block;
+
+  & input {
+    display: none;
+  }
+  & input:checked + span {
+    background-color: var(--bs-secondary);
+    border-color: var(--bs-primary);
+    color: var(--bs-primary);
+  }
+
+  & span {
+    display: block;
+    border: 1px solid #fff;
+    background-color: #fff;
+    border-radius: 12px;
+
+    cursor: pointer;
+    user-select: none;
+    font-size: 16px;
+    line-height: 1.5;
+    padding: 9px 11px;
+    color: #1d1d1d;
+    text-align: center;
+    white-space: nowrap;
+  }
+}
+</style>
