b8abfce740d8ab4ff2ce51a3aa99fedcf7fb5357
EQ0846 <skeq0846@skl.com.tw>
2022-05-29 13:42:22 +0800

    修改：熱賣推薦畫面調整

--- src/views/front/RecommendPage.vue
+++ src/views/front/RecommendPage.vue
@@ -2,13 +2,14 @@
 import { ref } from 'vue';
 
 // import PostCard from '@/components/front/PostCard.vue';
-// import DynamicWall from '@/components/front/DynamicWall.vue';
+import UserCard from '@/components/front/UserCard.vue';
+import ArticleCard from '@/components/front/ArticleCard.vue';
 import postsStore from '@/stores/postsStore';
 
 export default {
   components: {
-    // PostCard,
-    // DynamicWall,
+    UserCard,
+    ArticleCard,
   },
   setup() {
     const postsData = postsStore();
@@ -42,95 +43,34 @@ export default {
           <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
           <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
           <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
-          <span class="btn btn-xs btn-white me-3 mb-3">標籤文字</span>
         </div>
       </div>
       <div class="mb-3">
         <div class="title mb-3">本週熱門創作者</div>
-        <div class="row row-cols-5 gx-5">
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div class="img-fluid bg-primary mb-3 rounded tmp"></div>
-                <div>用戶名稱</div>
-                <div>
-                  <div class="icon">
-                    <i class="bi bi-heart"></i>
-                    <span>123</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+        <div class="row gy-5">
+          <div class="col-auto">
+            <UserCard />
           </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div class="img-fluid bg-primary mb-3 rounded tmp"></div>
-                <div>用戶名稱</div>
-                <div>
-                  <div class="icon">
-                    <i class="bi bi-heart"></i>
-                    <span>123</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="col-auto">
+            <UserCard />
           </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div class="img-fluid bg-primary mb-3 rounded tmp"></div>
-                <div>用戶名稱</div>
-                <div>
-                  <div class="icon">
-                    <i class="bi bi-heart"></i>
-                    <span>123</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="col-auto">
+            <UserCard />
           </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div class="img-fluid bg-primary mb-3 rounded tmp"></div>
-                <div>用戶名稱</div>
-                <div>
-                  <div class="icon">
-                    <i class="bi bi-heart"></i>
-                    <span>123</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="col-auto">
+            <UserCard />
           </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div class="img-fluid bg-primary mb-3 rounded tmp"></div>
-                <div>用戶名稱</div>
-                <div>
-                  <div class="icon">
-                    <i class="bi bi-heart"></i>
-                    <span>123</span>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
           </div>
         </div>
       </div>
@@ -138,42 +78,56 @@ export default {
         <div class="title mb-3">本週熱門文章</div>
         <div class="row row-cols-2 gx-5">
           <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Velit eum veniam nemo quidem, expedita enim iure dolore dicta
-                  ipsa excepturi ducimus voluptatem! Qui molestiae quos quas cum
-                  fugiat numquam pariatur!
-                </div>
-
-                <div>
-                  <div class="a">
-                    <i class="bi bi-heart"></i>
-                    <span class=""></span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ArticleCard />
           </div>
           <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <div>
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                  Velit eum veniam nemo quidem, expedita enim iure dolore dicta
-                  ipsa excepturi ducimus voluptatem! Qui molestiae quos quas cum
-                  fugiat numquam pariatur!
-                </div>
-
-                <div>
-                  <div class="a">
-                    <i class="bi bi-heart"></i>
-                    <span class=""></span>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <ArticleCard />
+          </div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <div class="title mb-3">熱門攝影創作者</div>
+        <div class="row gy-5">
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+        </div>
+      </div>
+      <div class="mb-3">
+        <div class="title mb-3">熱門文學創作者</div>
+        <div class="row gy-5">
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
+          </div>
+          <div class="col-auto">
+            <UserCard />
           </div>
         </div>
       </div>
