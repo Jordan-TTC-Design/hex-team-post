be76cf821ec8447cfdadc5261908c81cc89a1098
EQ0846 <skeq0846@skl.com.tw>
2022-06-05 14:41:49 +0800

    新增：追蹤名單，訂閱名單 Card

---
+++ src/components/front/cards/FollowingCard.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: ['following'],
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
+<template>
+  <div class="card mb-3">
+    <div class="card-body border-bottom">
+      <div class="card-title">追蹤名單</div>
+    </div>
+    <div class="card-body">
+      <div class="d-flex align-items-center mb-3" v-for="f in props.following" :key="f.id">
+        <div class="user-picture"></div>
+        <div class="user-info">
+          <RouterLink :to="`/profile/${f.id}`" class="user-info-title">{{
+            f.name
+          }}</RouterLink>
+        </div>
+        <!-- <div class="btn btn-sm text-primary">追蹤</div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/front/cards/ProductCard.vue
@@ -0,0 +1,34 @@
+<script>
+import MoreModel from '@/components/helper/MoreModel.vue';
+
+export default {
+  props: {
+    products: Array,
+  },
+  components: {
+    MoreModel,
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
+};
+</script>
+<template>
+  <div class="card mb-3">
+    <div class="card-body border-bottom">
+      <div class="card-title">訂閱方案</div>
+    </div>
+    <div class="card-body">
+      <div class="sponsor" v-for="p in props.products" :key="p._id">
+        <span class="sponsor-title">{{ p.coin }} SD / {{p.effectiveOfMonthNumber}} 月</span>
+        <span class="sponsor-subtitle">{{ p.name }}</span>
+      </div>
+    </div>
+  </div>
+  <MoreModel />
+</template>
+
+<style lang="scss" scoped>
+</style>
